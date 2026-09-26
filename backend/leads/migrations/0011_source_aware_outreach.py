1cacd7bb1b6d6ce41e80504fa79f98623ddc83c5
ssvprasad144 <ssvprasad144@gmail.com>
2026-09-26 15:31:26 +0530

    Store lead action URL in outreach migration

--- backend/leads/migrations/0011_source_aware_outreach.py
+++ backend/leads/migrations/0011_source_aware_outreach.py
@@ -5,6 +5,7 @@ class Migration(migrations.Migration):
     dependencies = [("leads", "0010_discovery_cost_intelligence")]
 
     operations = [
+        migrations.AddField(model_name="lead", name="action_url", field=models.URLField(blank=True)),
         migrations.AddField(model_name="outreach", name="medium", field=models.CharField(default="email", max_length=40, db_index=True)),
         migrations.AddField(model_name="outreach", name="action_type", field=models.CharField(default="send_email", max_length=50)),
         migrations.AddField(model_name="outreach", name="destination_url", field=models.URLField(blank=True)),
